3a78fe33de83279af5622484c56a1bb75dba96b4
Magucero <s.pereyragallardo@itecriocuarto.org.ar>
2026-04-01 21:49:57 -0300

    cambios en saludo minimos

--- saludo/views.py
+++ saludo/views.py
@@ -1,15 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-
+contexto = {"nombre":"juancito",
+            "esMayor":True,
+            "mascotas": ["firu","boby", "michigan"]
+                }
 # Create your views here.
 def saludo(request):
-    return HttpResponse("<h1> Hola mundor</h1>")
-
-
+    return render(request,'saludo/index.html',contexto)
 #nota establecer una ruta
 
 def despedir(request):
-    return HttpResponse("<h1> chau mundor</h1>")
+    return render(request,"saludo/despedir.html",contexto)
 
 def inicio(request):
     return HttpResponse("<h1> estoy en root</h1>")
+
+
+#nuevo proyecto
+
