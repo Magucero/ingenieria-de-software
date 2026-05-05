ac803d98bf2fe93a3064a262d0c9cea3fe6d7098
Magucero <s.pereyragallardo@itecriocuarto.org.ar>
2026-05-05 20:14:08 -0300

    4-may

--- todolist/urls.py
+++ todolist/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.tareas, name="tareas"),
     path("nueva/", views.crear_tarea, name="crear_tarea"),
+    path("editar/<int:id>/", views.editar_tarea, name="editar"),
+    path("borrar/<int:id>/", views.eliminar_tarea, name="borrar"),
 
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Tarea
 from .forms import TareaForm
@@ -13,6 +13,7 @@ def tareas(request):
 def crear_tarea(request):
     if request.method == "POST":
         form = TareaForm(request.POST)
+        
         if form.is_valid():
             form.save()
             return redirect('tareas')
@@ -21,40 +22,59 @@ def crear_tarea(request):
     
     return render(request, 'todolist/crear_tarea.html', {"form":form})
     
+def editar_tarea(request,id):
+    tarea = get_object_or_404(Tarea, id=id)
+    
+    if request.method == "POST":
+        form = TareaForm(request.POST,instance=tarea)
+        if form.is_valid():
+            form.save()
+            return redirect('tareas')
+            
+    else:
+        form = TareaForm(instance=tarea)
+
+    return render(request,"todolist/editar_tarea.html",{"form":form})    
+
+    
+def eliminar_tarea(request,id):
+    tarea = get_object_or_404(Tarea, id=id)
+
+    if request.method == 'POST':
+        tarea.delete()
+        return redirect(tareas)
+    return render(request,"todolist/borrar_tarea.html",{"tarea":tarea})
 
 
 
+# """
+
+#     # LOOKUPS - obtención de datos desde el orm
 
 
+#     #get by id
+#     tareas = Tarea.objects.get(id=2)
 
-"""
-
-    # LOOKUPS - obtención de datos desde el orm
-
-
-    #get by id
-    tareas = Tarea.objects.get(id=2)
-
-    #str
-    tareas = Tarea.objects.filter(nombre__icontains="bugfix")
+#     #str
+#     tareas = Tarea.objects.filter(nombre__icontains="bugfix")
 
 
-    #int y floats
-    posteos = Posteos.objects.filter(likes__gt=10)
-    posteos = Posteos.objects.filter(likes__gte=10)
-    posteos = Posteos.objects.filter(likes__lt=100)
-    posteos = Posteos.objects.filter(likes__lte=100)
-    posteos = Posteos.objects.filter(likes__range=(10,100))
+#     #int y floats
+#     posteos = Posteos.objects.filter(likes__gt=10)
+#     posteos = Posteos.objects.filter(likes__gte=10)
+#     posteos = Posteos.objects.filter(likes__lt=100)
+#     posteos = Posteos.objects.filter(likes__lte=100)
+#     posteos = Posteos.objects.filter(likes__range=(10,100))
 
-    #listas
-    tareas = Tarea.objects.filter(etiquetas__in=["Urgente",2025])
+#     #listas
+#     tareas = Tarea.objects.filter(etiquetas__in=["Urgente",2025])
 
-    #fecha
-    tareas = Tarea.objects.filter(fecha_completado__year=2025)
-    tareas.filter(fecha_completado__month=3)
+#     #fecha
+#     tareas = Tarea.objects.filter(fecha_completado__year=2025)
+#     tareas.filter(fecha_completado__month=3)
 
-    #saber si en nulo
-    tareas = Tarea.objects.filter(responsable__isnull=False)
+#     #saber si en nulo
+#     tareas = Tarea.objects.filter(responsable__isnull=False)
 
-    tareas = Tarea.objects.filter(responsable__username="Facundo")
-"""
+#     tareas = Tarea.objects.filter(responsable__username="Facundo")
+# """
